65154af6efc4ea3ecb416af14ab3c4884164a63e
mtater95 <mtater95@gmail.com>
2025-07-29 20:32:50 -0600

    Delete example_parent_checkin.js

--- example_parent_checkin.js
+++
@@ -1,5 +0,0 @@
-function checkInKid(name) {
-  let kids = JSON.parse(localStorage.getItem('checkedInKids') || '[]');
-  kids.push({ name });
-  localStorage.setItem('checkedInKids', JSON.stringify(kids));
-}
